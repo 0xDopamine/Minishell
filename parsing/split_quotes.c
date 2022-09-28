616c1765b80a917f92d473d78e48f458bc871e37
0xDopamine <aminebumi1@gmail.com>
2022-09-28 05:02:38 +0100

    done for today

--- parsing/split_quotes.c
+++ parsing/split_quotes.c
@@ -6,100 +6,12 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 21:19:11 by mbaioumy          #+#    #+#             */
-/*   Updated: 2022/09/28 03:31:25 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2022/09/28 05:01:24 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 #include "exec.h"
-
-// int	ft_qword_count(char *str, char sep)
-// {
-// 	int	words;
-// 	int	i;
-// 	int	tok;
-
-// 	i = 0;
-// 	words = 1;
-// 	while (str[i++])
-// 	{
-// 		if (str[i] && str[i] == sep)
-// 		{
-// 			i++;
-// 			words++;
-// 		}
-// 		if (ft_strchr(str[i], "\'\""))
-// 		{
-// 			tok = str[i];
-// 			while (str[i] && str[i] != tok)
-// 				i++;
-// 		}
-// 		else if (str[i])
-// 			i++;
-// 	}
-// 	return (words);
-// }
-
-// int	ft_word_len(char *str, char sep)
-// {
-// 	int	len;
-
-// 	len = 0;
-// 	while (str[len])
-// 	{
-// 		if (str[len] == sep)
-// 			break ;
-// 		if (ft_strchr(str[len], "\'\""))
-// 		{
-// 			len++;
-// 			while (str[len] && !ft_strchr(str[len], "'\""))
-// 				len++;
-// 		}
-// 		else
-// 			len++;
-// 	}
-// 	return (len);
-// }
-
-// char	**ft_split_string(char *str, char **split, char sep)
-// {
-// 	t_split	data;
-
-// 	data.i = 0;
-// 	data.j = -1;
-// 	while (str[data.i])
-// 	{
-// 		while (str[data.i] && str[data.i] == sep)
-// 			data.i++;
-// 		data.len = ft_word_len(str, sep);
-// 		split[++data.j] = ft_calloc(data.len + 2, sizeof(char));
-// 		ft_split_protection(split);
-// 		data.k = -1;
-// 		while (++data.k < data.len && str[data.i] && str[data.i] != sep)
-// 		{
-// 			if (ft_strchr(str[data.i], "\'\""))
-// 				ft_quotes_case(split, str, data);
-// 			split[data.j][data.k] = str[data.i++];
-// 		}
-// 		split[data.j][data.k] = '\0';
-// 		if (str[data.i])
-// 			data.i++;
-// 	}
-// 	split[data.j + 1] = NULL;
-// 	return (split);
-// }
-
-// char	**ft_split_q(char *str, char sep)
-// {
-// 	char	**split;
-// 	char	**ret;
-
-// 	split = ft_calloc((ft_qword_count(str, sep) + 2), sizeof(char *));
-// 	if (!split)
-// 		return (NULL);
-// 	ret = ft_split_string(str, split, sep);
-// 	return (ret);
-// }
 
 int	ft_qword_count(char *str, char sep)
 {
@@ -170,7 +82,8 @@ char	**ft_split_string(char *str, char **split, char sep, t_split *data)
 				data->tok = str[data->i];
 				split[data->j][data->k++] = str[data->i];
 				data->i++;
-				while (data->k < data->len && str[data->i] && str[data->i] != data->tok)
+				while (data->k < data->len && str[data->i]
+					&& str[data->i] != data->tok)
 					split[data->j][data->k++] = str[data->i++];
 			}
 			split[data->j][data->k] = str[data->i++];
@@ -180,8 +93,6 @@ char	**ft_split_string(char *str, char **split, char sep, t_split *data)
 			data->i++;
 	}
 	split[data->j + 1] = NULL;
-	// printf("%s\n", split[0]);
-	// printf("%s\n", split[1]);
 	return (split);
 }
 
